f30f945f031871e0889c4a29de4dede00739d258
Sinnafugl <steffenvetrhus@hotmail.com>
2015-03-15 05:55:46 +0100

    Api funker

--- BlankCordovaApp1/BlankCordovaApp1/controllers/controllers.js
+++ BlankCordovaApp1/BlankCordovaApp1/controllers/controllers.js
@@ -5,6 +5,7 @@
         $scope.deleteTrip = function (idx) {
             $scope.previousTrips.splice(idx, 1);
         }
+        
 
         $scope.toggleTrip = function (trip) {
             if ($scope.isTripShown(trip)) {
@@ -16,26 +17,68 @@
         $scope.isTripShown = function (trip) {
             return $scope.shownTrip === trip;
         };
+        
 
     }])
-    .controller('menuCtrl', ['$scope', 'lStorage', function ($scope, lStorage) {
+    .controller('menuCtrl', ['$scope', '$http', 'lStorage', function ($scope, $http, lStorage) {
         $scope.trips = lStorage.getAll('trips')
+        
+
+        
+        $http({
+            url: 'http://nettverkkrs.no:5000/buss',
+            method: 'POST',
+            data: $scope.trips[$scope.trips.length -1],
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).success(function (data, status, headers, config) {
+            $scope.tripps = data;
+            console.log('Success')
+            console.log($scope.tripps);
+                
+        }).error(function (data, status, headers, config) {
+            $scope.status = status;
+            console.log(data, status, headers, config)
+        })
+        
+        
+    
+        
+        /*$scope.uniqueTripps = $scope.tripps.filter(function (item, pos, self) {
+            return self.indexOf(item) == pos;
+            console.log($scope.uniqueTripps)
+        })*/
+    
+
 
         $scope.sendTrip = function (trip) {
             var newTrip = {
-                from: trip.from,
+                fra: trip.fra,
                 to: trip.to,
                 time: trip.time,
                 date: trip.date,
-                type: trip.type
+                type: trip.type,
+                direction: "1"
+                
             }
             trip.from = '';
             trip.to = '';
             trip.time = '';
             trip.date = '';
+            trip.direction = '';
             $scope.trips.push(newTrip)
-            lStorage.save('trips', $scope.trips)
+            lStorage.save('trips', $scope.trips) = JSON.stringify('trips')
         }
+
+       /* $http({
+            method: 'GET',
+            url: 'http://nettverkkrs.no:5000/buss',
+            params: $rootScope.testTrip,
+        }).success(function (data) {
+            $scope.trip = data;
+            console.log(data)
+        }) */
         function menuToggle($scope, $ionicSideMenuDelegate) {
             $scope.toggleLeft = function () {
                 $ionicSideMenuDelegate.toggleLeft();
